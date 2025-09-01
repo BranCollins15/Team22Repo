e0c78cde5447bdf8b496aac8e66127eca9f1e0c6
A-Quanta-Of-Light <195991695+A-Quanta-Of-Light@users.noreply.github.com>
2025-08-31 23:32:50 -0400

    Add files via upload

---
+++ LAMPAPI/SignUp.php
@@ -0,0 +1,41 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$login = $inData["login"];
+	$passwordHash = $inData["passwordHash"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "Database1");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, PasswordHash) VALUES(?,?,?,?)");
+		$stmt->bind_param("ssss", $firstName, $lastName, $login, $passwordHash);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
